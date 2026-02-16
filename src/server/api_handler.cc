10d21320e79d0c5dd96ddef5bafcf5ba27f7d01e
Simon Fuhrmann <simonfuhrmann@gmx.de>
2026-02-16 13:32:25 -0800

    Add /api/photo request to ApiHandler

--- src/server/api_handler.cc
+++ src/server/api_handler.cc
@@ -7,8 +7,11 @@
 #include <vector>
 
 #include "src/server/net/http_req_target.h"
+#include "src/server/util/base64.h"
 #include "src/server/util/file_utils.h"
 #include "src/server/util/json.h"
+#include "src/server/util/status_or.h"
+#include "src/server/xmp_util.h"
 
 namespace server {
 namespace {
@@ -48,32 +51,42 @@ util::Status ApiHandler::Handle(net::HttpRequest& request) const {
 
   if (target.GetPath() == "/api/album") {
     return HandleAlbumRequest(request, target);
+  } else if (target.GetPath() == "/api/photo") {
+    return HandlePhotoRequest(request, target);
   }
 
   return util::AbortedError("Invalid API request");
 }
 
+util::StatusOr<std::string> ApiHandler::GetLocalPath(
+    std::string_view req_path) const {
+  // Normalize the path and ensure it is under the photos root directory.
+  std::string local_path = util::GetNormalizedPath(
+      util::StrCat(options_.photos_root, "/", req_path));
+  if (!util::HasPrefix(local_path, options_.photos_root)) {
+    return util::AbortedError("Invalid path");
+  }
+  // Make sure the path exists in the file system.
+  if (!fs::exists(local_path)) {
+    return util::AbortedError("Path not found");
+  }
+  return local_path;
+}
+
 util::Status ApiHandler::HandleAlbumRequest(
     net::HttpRequest& request, const net::HttpReqTarget& target) const {
   const std::string_view album_path = target.GetParam("path");
-  const std::string full_path =
-      util::StrCat(options_.photos_root, "/", std::string(album_path));
+  ASSIGN_OR_RETURN(const std::string local_path, GetLocalPath(album_path));
 
-  // Normalize the path and ensure it is under the photos root directory.
-  const std::string normalized_path = util::GetNormalizedPath(full_path);
-  if (!util::HasPrefix(normalized_path, options_.photos_root)) {
-    return util::AbortedError("Invalid album path");
-  }
-
-  // Check if the album path exists and is a directory.
-  if (!fs::exists(normalized_path) || !fs::is_directory(normalized_path)) {
+  // Check if the album path is a directory.
+  if (!fs::is_directory(local_path)) {
     return util::AbortedError("Album not found");
   }
 
   // Read the directory contents.
   std::vector<std::string> albums;
   std::vector<std::string> photos;
-  for (const auto& entry : fs::directory_iterator(normalized_path)) {
+  for (const auto& entry : fs::directory_iterator(local_path)) {
     if (entry.is_directory()) {
       albums.push_back(entry.path().filename().string());
     } else if (IsImageFile(entry)) {
@@ -107,10 +120,86 @@ util::Status ApiHandler::HandleAlbumRequest(
   // Send the response as a JSON object.
   request.SetReplyStatus(net::HttpStatus::CODE_200_OK);
   request.SetReplyContentType("application/json");
-  for (const auto& [name, value]: options_.reply_headers) {
+  for (const auto& [name, value] : options_.reply_headers) {
     request.SetReplyHeader(name, value);
   }
   request.SetReplyBody(json.Serialize(), /*copy_data=*/true);
+  return request.Reply();
+}
+
+util::Status ApiHandler::HandlePhotoRequest(
+    net::HttpRequest& request, const net::HttpReqTarget& target) const {
+  const std::string_view eye = target.GetParam("eye");
+  if (eye != "left" && eye != "right") {
+    return util::AbortedError("Invalid eye parameter");
+  }
+  // Get the local path of the photo.
+  const std::string_view req_path = target.GetParam("path");
+  ASSIGN_OR_RETURN(const std::string local_path, GetLocalPath(req_path));
+
+  const bool is_left_eye = (eye == "left");
+  return is_left_eye ? HandlePhotoLeftEyeRequest(request, local_path)
+                     : HandlePhotoRightEyeRequest(request, local_path);
+}
+
+util::Status ApiHandler::HandlePhotoLeftEyeRequest(
+    net::HttpRequest& request, std::string_view local_path) const {
+  // For the left eye, just return the original JPEG data. It would be nice
+  // to strip the extra XMP metadata for the right eye to save bandwidth.
+  ASSIGN_OR_RETURN(const std::string photo_data, util::ReadFile(local_path));
+  request.SetReplyStatus(net::HttpStatus::CODE_200_OK);
+  request.SetReplyContentType("image/jpeg");
+  for (const auto& [name, value] : options_.reply_headers) {
+    request.SetReplyHeader(name, value);
+  }
+  request.SetReplyBody(photo_data, /*copy_data=*/false);
+  return request.Reply();
+}
+
+util::Status ApiHandler::HandlePhotoRightEyeRequest(
+    net::HttpRequest& request, std::string_view local_path) const {
+  // For the right eye, read the full JPEG, find the base64 encoded JPEG in the
+  // XMP metadata, decode it and return the data.
+  ASSIGN_OR_RETURN(const std::string photo_data, util::ReadFile(local_path));
+  ASSIGN_OR_RETURN(std::string xmp_data, ExtractXmpFromJpeg(photo_data));
+
+  // Extract the base64-encoded JPEG in the GImage:Data field.
+  constexpr std::string_view kXmpDataPrefix = "GImage:Data=\"";
+  const size_t xmp_pos = xmp_data.find(kXmpDataPrefix);
+  if (xmp_pos == std::string_view::npos) {
+    return util::AbortedError("No embedded data found in the photo");
+  }
+
+  // Compute the base64 start and end position in the XMP data.
+  const size_t base64_start = xmp_pos + kXmpDataPrefix.size();
+  size_t base64_end = xmp_data.find_first_of('"', base64_start);
+  if (base64_end == std::string_view::npos) {
+    return util::AbortedError("Invalid XMP metadata format");
+  }
+
+  // Some JPEG/XMP encoders do not properly pad the base64 data to multiples
+  // of 4 bytes. Sneak in the padding, overwriting original data in place (the
+  // data is not used otherwise).
+  while ((base64_end - base64_start) % 4 != 0) {
+    if (base64_end + 1 >= xmp_data.size()) break;
+    xmp_data[base64_end] = '=';
+    base64_end += 1;
+  }
+
+  // Decode the base64 data.
+  const std::string_view xmp_data_view(xmp_data);
+  const std::string_view base64_data =
+      xmp_data_view.substr(base64_start, base64_end - base64_start);
+  ASSIGN_OR_RETURN(const std::string decoded_data,
+                   util::Base64Decode(base64_data));
+
+  // Send the binary JPEG data as the response.
+  request.SetReplyStatus(net::HttpStatus::CODE_200_OK);
+  request.SetReplyContentType("image/jpeg");
+  for (const auto& [name, value] : options_.reply_headers) {
+    request.SetReplyHeader(name, value);
+  }
+  request.SetReplyBody(decoded_data, /*copy_data=*/false);
   return request.Reply();
 }
 
--- src/server/api_handler.h
+++ src/server/api_handler.h
@@ -2,10 +2,13 @@
 #define SRC_SERVER_API_HANDLER_H_
 
 #include <map>
+#include <string>
+#include <string_view>
 
 #include "src/server/net/http_handler.h"
 #include "src/server/net/http_req_target.h"
 #include "src/server/util/status.h"
+#include "src/server/util/status_or.h"
 
 namespace server {
 
@@ -23,9 +26,27 @@ namespace server {
 //     "entries": [
 //       { type: "album", "name": "Iceland" },
 //       { type: "album", "name": "Japan" },
-//       { type: "photo", "name": "cover.jpg", "proj": "vr180" }
+//       { type: "photo", "name": "cover.jpg", "eyes": "separate" }
 //     ]
 //   }
+//
+// Currently supported layouts for photos are
+//
+// - "separate": Google's VR photo format, which has the left eye in the JPEG
+//   data, the right eye in the XMP metadata as base64-encoded JPEG.
+//   https://developers.google.com/vr/reference/cardboard-camera-vr-photo-format
+// - "sbs": Side-by-side format, where the left and right eyes are stored in the
+//   same JPEG image, left and right half are the cooresponding eye.
+//
+// The renderer in the client can map each side of an SBS image to a texture
+// for rendering. However, for separate images, the client does not have easy
+// access to the XMP metadata; thus the API offers requests to get the left and
+// right eye images separately.
+//
+//   GET /api/photo?path=Travel/Iceland/cover.jpg&eye=left
+//   GET /api/photo?path=Travel/Iceland/cover.jpg&eye=right
+//
+// This API request will fail if the JPEG does not contain embedded XMP data.
 class ApiHandler : public net::HttpHandlerBase {
  public:
   struct Options {
@@ -39,8 +60,16 @@ class ApiHandler : public net::HttpHandlerBase {
   util::Status Handle(net::HttpRequest& request) const override;
   util::Status HandleAlbumRequest(net::HttpRequest& request,
                                   const net::HttpReqTarget& target) const;
+  util::Status HandlePhotoRequest(net::HttpRequest& request,
+                                  const net::HttpReqTarget& target) const;
+  util::Status HandlePhotoLeftEyeRequest(net::HttpRequest& request,
+                                         std::string_view local_path) const;
+  util::Status HandlePhotoRightEyeRequest(net::HttpRequest& request,
+                                          std::string_view local_path) const;
 
  private:
+  util::StatusOr<std::string> GetLocalPath(std::string_view req_path) const;
+
   const Options options_;
 };
 

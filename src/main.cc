ccbf799fc59f6a89a5614c63ea57b63a49044e6c
Simon Fuhrmann <simonfuhrmann@gmx.de>
2026-03-23 22:10:22 -0700

    Use argument parser in main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,28 @@
 #include <cstdlib>
 #include <iostream>
 
+#include "src/server/util/argument_parser.h"
 #include "src/server/util/status.h"
 #include "src/server/web_server.h"
 
-int main(int argc, char** argv) {
+int main(int argc, const char** argv) {
+  const util::ArgumentParser::Spec spec = {
+      .options = {{.long_name = "server-port",
+                   .has_value = true,
+                   .description = "Server port to listen on",
+                   .default_value = "8080"}},
+      .min_positional_args = 1,
+      .max_positional_args = 1,
+      .print_help_on_error = true,
+      .usage = "[options] <media_root>",
+  };
+  const auto arg_parser = util::ArgumentParser::CreateOrDie(spec);
+  const util::ParsedArguments args = arg_parser.ParseOrDie(argc, argv);
+
   server::WebServer::Options options;
-  options.listen_port = 8080;
+  options.listen_port = args.GetOptionAsIntOrDie("server-port");
+  options.media_root = args.GetPositionalOrDie(0);
   options.num_threads = 4;
-  options.media_root = "/data/vr180/";
   server::WebServer web_server(options);
   const util::Status status = web_server.Start();
   if (!status.ok()) {
